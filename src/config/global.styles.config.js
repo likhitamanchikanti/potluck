875bc62453b54e758aa19c044e119b801b2844d1
Likhita Manchikanti <likhi.2201@gmail.com>
2023-03-23 15:53:37 -0400

    created global styles config including sitewide colorscheme

---
+++ src/config/global.styles.config.js
@@ -0,0 +1,12 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import React from "react";
+
+export const color = {
+  orange: '#e04c1b',
+  black: '#131200',
+  green: '#78bc61',
+  lightGreen: '#c0c781',
+  blue: '#2978a0',
+  white: '#ffffff',
+}
